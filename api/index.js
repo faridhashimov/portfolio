40c16fcf20057bfdfb914c02df1e85df79a5685f
Farid <fargmov@gmail.com>
2022-09-02 20:52:48 +0300

    change project structure

--- api/index.js
+++ api/index.js
@@ -20,10 +20,10 @@ app.use('/api', projectRoute)
 
 // Serve frontend
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../build')))
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, '../','frontend', 'build', 'index.html'))
     })
 } else {
     app.get('/', (req, res) => {
