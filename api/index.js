7a91e43f0a16e4a6719e8d5df1fbf0fd793e8688
Farid <fargmov@gmail.com>
2022-12-07 14:24:26 +0300

    code refactore for successfull deploy #1

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
-import * as path from 'path'
-import { fileURLToPath } from 'url'
+// import * as path from 'path'
+// import { fileURLToPath } from 'url'
 import express from 'express'
 import dotenv from 'dotenv'
 import cors from 'cors'
@@ -8,8 +8,8 @@ import projectRoute from './routes/projectRoute.js'
 dotenv.config()
 dbConfig()
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+// const __filename = fileURLToPath(import.meta.url)
+// const __dirname = path.dirname(__filename)
 
 const app = express()
 
@@ -19,17 +19,17 @@ app.use(cors())
 app.use('/api', projectRoute)
 
 // Serve frontend
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/build')))
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../','frontend', 'build', 'index.html'))
-    })
-} else {
-    app.get('/', (req, res) => {
-        res.send('Please set to production!')
-    })
-}
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, '../','frontend', 'build', 'index.html'))
+//     })
+// } else {
+//     app.get('/', (req, res) => {
+//         res.send('Please set to production!')
+//     })
+// }
 
 app.listen(process.env.PORT || 5000, () => {
     console.log(`Connected to backend`)
